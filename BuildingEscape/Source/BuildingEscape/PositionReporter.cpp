78f75674ba076a4280ae79242ad7d7dbf429f353
Alexis Morel <alexismoreldev@gmail.com>
2019-05-10 21:10:36 -0400

    new map, 2 chairs and PositionReporter class creation

---
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -0,0 +1,31 @@
+#include "PositionReporter.h"
+
+// Sets default values for this component's properties
+UPositionReporter::UPositionReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	m_actor = GetOwner();
+}
+
+
+// Called when the game starts
+void UPositionReporter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("PositionReporter initialization of %s..."), *m_actor->GetName());
+	
+}
+
+
+// Called every frame
+void UPositionReporter::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
